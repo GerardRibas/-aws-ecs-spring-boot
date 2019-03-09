553f65a9767f266fe20744b01bb354ff5daf1d74
Gerard Ribas <gerard.ribas.canals@gmail.com>
2019-03-09 17:14:00 +0100

    Added logback and slf4j to the endpoint

--- src/main/java/cat/grc/aws/ecs/controller/HelloController.java
+++ src/main/java/cat/grc/aws/ecs/controller/HelloController.java
@@ -2,6 +2,7 @@ package cat.grc.aws.ecs.controller;
 
 import cat.grc.aws.ecs.dto.HelloDto;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
+@Slf4j
 public class HelloController {
 
     private static final String template = "Hello, %s!";
@@ -16,8 +18,10 @@ public class HelloController {
 
     @GetMapping("/hello")
     public HelloDto sayHello(@RequestParam(value="name", defaultValue="World") String name) {
+        long id = counter.incrementAndGet();
+        log.info("Saying hello with id={} and name={}", id, name);
         return HelloDto.builder()
-                .id(counter.incrementAndGet())
+                .id(id)
                 .content(String.format(template, name))
                 .build();
     }
